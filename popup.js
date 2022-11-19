03fc7a48ef3517a640181dcb211778b59580ebad
tyrants666 <akashyadav.iiita@gmail.com>
2022-11-19 23:20:45 +0545

    Urls saved to Object & Workspace name added to UI

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 
-const tabs = await chrome.tabs.query({ currentWindow:true });
-// console.log(tabs);
 
+const tabs = await chrome.tabs.query({ currentWindow:true });
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
@@ -26,9 +25,70 @@ for (const tab of tabs) {
 }
 document.querySelector("ul").append(...elements);
 
+//Add Group Onclick
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
+
+  //Group Tabs
   const tabIds = tabs.map(({ id }) => id);
   const group = await chrome.tabs.group({ tabIds });
   await chrome.tabGroups.update(group, { title: "DOCS" });
+
+  //Save Workkspace
+  let wsObj = new workspaceClass()
+  wsObj.save_workspace();
+
 });
+
+// CLASS WOKSPACE -------------------------
+class workspaceClass {
+  constructor(){
+    this.wsName = document.querySelector('#workspace-name').value;
+  }
+
+  save_workspace(){
+    if(this.wsName){
+      let workspaces = new Object();
+      workspaces.workspace_name = this.wsName;
+      workspaces.workspace_color = 'red';
+      workspaces.urls = {}
+      for (const i in tabs) {
+        workspaces.urls[i] = {
+          title:tabs[i].title,
+          icon:tabs[i].favIconUrl,
+          url:tabs[i].url
+        }
+      }
+      this.create_workspace_elem();
+      console.log(workspaces);
+    }
+  }
+
+  create_workspace_elem() {
+    const wsWrapper = document.querySelector('.workspaces');
+    const wsTemplate = document.querySelector('#ws_template');
+    // const elements = new Set();
+    const ws = wsTemplate.content.firstElementChild.cloneNode(true)
+    ws.querySelector('.ws-name').textContent = this.wsName;
+    // elements.add(element);
+    wsWrapper.append(ws)
+  }
+
+}
+
+// workspaces = {
+//   workspace_name: "HP Tester",
+//   workspace_color: "Red",
+//   urls:{
+//     0:{
+//       icon:"",
+//       name:"demo9",
+//       url:"https://demo9.staging.com/"
+//     },
+//     1:{
+//       icon:"",
+//       name:"demo9",
+//       url:"https://demo9.staging.com/"
+//     }
+//   }
+// }
